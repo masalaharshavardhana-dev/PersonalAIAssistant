0a64d5f243066b7372c08453cc6afd5d96f69dc5
masalaharshavardhana-dev <masalaharshavardhan@gmail.com>
2026-03-10 15:53:41 +0530

    updated code

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ components.html(
                         
                         const text = btn.innerText.trim();
                         if (text === 'Record' || text === '🎙️') {
-                            btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" viewBox="0 0 16 16"><rect x="7" y="3" width="2" height="10" rx="1"/><rect x="11" y="5" width="2" height="6" rx="1"/><rect x="3" y="5" width="2" height="6" rx="1"/></svg>';
+                            btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" viewBox="0 0 16 16"><path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0z"/><path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"/></svg>';
                         } else if (text === '🛑') {
                             btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" viewBox="0 0 16 16"><rect width="10" height="10" x="3" y="3" rx="2"/></svg>';
                         }
